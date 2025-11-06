92e387cb03f7d084c4a5661aa6707b9681035473
Vishu Verma <visheshbaund@gmail.com>
2025-11-06 11:58:16 +0530

    Try of fix error occur during deployment | inngest error.

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,6 +2,8 @@ import { inngest } from "@/lib/inngest/client";
 import { generateIndustryInsights, helloWorld } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
+export const dynamic = "force-dynamic";
+
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
